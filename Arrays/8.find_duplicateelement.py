12a13174719560a6c977616060d79aa84a03cda6
Tanujarora100 <108284546+Tanujarora100@users.noreply.github.com>
2023-01-06 00:47:49 +0530

    Create 8.find_duplicateelement.py

---
+++ Arrays/8.find_duplicateelement.py
@@ -0,0 +1,54 @@
+class solution:
+    from ast import List
+
+    # Using set to find the duplicate element
+    '''
+    Time Complexity: O(n)O(n)
+
+    HashSet insertions and lookups have amortized constant time complexities.
+    Hence, this algorithm requires linear time,
+    since it consists of a single for loop that iterates over each element,
+    looking up the element and inserting it into the set at most once.
+    Space Complexity: O(n)
+    We use a set that may need to store at most n element
+    leading to a linear space complexity of O(n).
+    '''
+
+    def findDuplicate(self, nums: List[int]) -> int:
+        dup = set()
+        res = 0
+        for i in range(len(nums)):
+            if nums[i] not in dup:
+                dup.add(nums[i])
+            else:
+                res = nums[i]
+        return res
+
+    # Time Complexity-O(NLOGN)
+    # Space Complexity-O(N) Tim sort
+
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 0
+        for i in range(1, len(nums)):
+            if nums[i-1] == nums[i]:
+                res = nums[i]
+        return res
+
+# Flyod cycle detection
+    def findDuplicate(self, nums):
+        # Find the intersection point of the two runners.
+        tortoise = hare = nums[0]
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
+
+        # Find the "entrance" to the cycle.
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+
+        return hare
