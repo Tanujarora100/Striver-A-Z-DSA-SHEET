6382b07804068c1c293ee40e447cadbac45a351c
Tanujarora100 <108284546+Tanujarora100@users.noreply.github.com>
2023-01-10 16:55:14 +0530

    Update 9-celebrity_problem.py

--- StackandQueue/9-celebrity_problem.py
+++ StackandQueue/9-celebrity_problem.py
@@ -1,39 +1,41 @@
 class solution:
-    def celebrity(self, M, n):
-        stack = []
-
-        for i in range(n):
-            stack.append(i)
-
-        while len(stack) != 1:
-            a = stack.pop()
-            b = stack.pop()
-            if self.knows(a, b, M):
-                stack.append(b)
+    def findCelebrity(n, knows):
+        # Create a stack and push all ids in it.
+        ids = []
+        for i in range(0, n):
+            ids.append(i)
+        #  Finding celebrity.
+        while (len(ids) > 1):
+            id1 = ids.pop()
+            id2 = ids.pop()
+            if knows(id1, id2) == True:
+                # Because person with id1 can not be celebrity.
+                ids.append(id2)
             else:
-                stack.append(a)
-        candidate = stack[-1]
+                # Because person with id2 can not be celebrity.
+                ids.append(id1)
 
-        # Verify now
+        celebrity = ids[len(ids)-1]
 
-        zeroCount = 0
-        for i in range(n):
-            if M[candidate][i] == 0:
-                zeroCount += 1
+        knowAny = False
+        knownToAll = True
+        # Verify whether the celebrity knows any other person.
+        for i in range(0, n):
+            if knows(celebrity, i) == True:
+                knowAny = True
+                break
 
-        if zeroCount != n:
-            return -1
+        # Verify whether the celebrity is known to all the other person.
+        for i in range(0, n):
+            if i != celebrity and knows(i, celebrity) == False:
+                knownToAll = False
+                break
 
-        one_count = 0
+        if knowAny != False or knownToAll == False:
+            # If verificatin failed, then it means there is no celebrity at the party.
+            celebrity = -1
 
-        for i in range(n):
-            if M[i][candidate] == 1:
-                one_count += 1
-
-        if one_count != n-1:
-            return -1
-
-        return candidate
+        return celebrity
 
     def knows(self, a, b, M):
         if M[a][b] == 1:
@@ -41,21 +43,63 @@ class solution:
         else:
             return False
 
-    def celebrityTwoPointer(self, M, n):
-        # code here
-        i = 0
-        j = n-1
-        candidate = -1
-        while (i < j):
-            if M[j][i] == 1:
-                j -= 1
+    def findCelebrity(n, knows):
+        # Two pointers pointing at start and end of search space.
+        p = 0
+        q = n-1
+        while (p < q):
+            # This means p cannot be celebrity.
+            if knows(p, q) == True:
+                p += 1
+            # This means q cannot be celebrity.
             else:
-                i += 1
+                q -= 1
 
-        candidate = i
-        for k in range(n):
-            if candidate != k:
-                if M[candidate][k] == 1 or M[k][candidate] == 0:
-                    return -1
+        celebrity = p
+        knowAny = False
+        knownToAll = True
+        # Verify whether the celebrity knows any other person.
+        for i in range(0, n):
+            if knows(celebrity, i) == True:
+                knowAny = True
+                break
+        # Verify whether the celebrity is known to all the other person.
+        for i in range(0, n):
+            if i != celebrity and knows(i, celebrity) == False:
+                knownToAll = False
+                break
 
-        return candidate
+        if knowAny != False or knownToAll == False:
+            # If verificatin failed, then it means there is no celebrity at the party.
+            celebrity = -1
+
+        return celebrity
+
+
+"""
+    Time complexity: O(N * N)
+    Space complexity: O(N)
+	
+    Where ‘N’ is the number of people at the party.
+"""
+
+
+def findCelebrity(n, knows):
+    #  Calculating indegree and outdegree of each nodes.
+    indegree = [0] * n
+    outdegree = [0] * n
+
+    for i in range(0, n):
+        for j in range(0, n):
+            if knows(i, j) == 1:
+                indegree[j] += 1
+                outdegree[i] += 1
+
+    # Finding Celebrity
+    celebrity = -1
+    for i in range(0, n):
+        if indegree[i] == n - 1 and outdegree[i] == 0:
+            celebrity = i
+            break
+
+    return celebrity
