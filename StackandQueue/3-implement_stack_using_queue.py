e3b5f47d073cfaf3b78c2e7382e7690c80754332
Tanujarora100 <108284546+Tanujarora100@users.noreply.github.com>
2023-01-10 09:31:37 +0530

    Update 3-implement_stack_using_queue.py

--- StackandQueue/3-implement_stack_using_queue.py
+++ StackandQueue/3-implement_stack_using_queue.py
@@ -8,19 +8,19 @@ class MyStack:
         self._size_stack = 0
 
     def push(self, x: int) -> None:
-        self._stack.appendleft(x)
+        self._stack.append(x)
         self._size_stack += 1
 
     def pop(self) -> int:
         if self._size_stack <= 0:
             return -1
         else:
-            popped_ele = self._stack.popleft()
+            popped_ele = self._stack.pop()
             self._size_stack -= 1
             return popped_ele
 
     def top(self) -> int:
-        return self._stack[0]
+        return self._stack[-1]
 
     def empty(self) -> bool:
         if self._size_stack <= 0:
@@ -28,5 +28,5 @@ class MyStack:
         else:
             return False
 
-    def length(self) -> int:
-        return self._size_stack
+
+
