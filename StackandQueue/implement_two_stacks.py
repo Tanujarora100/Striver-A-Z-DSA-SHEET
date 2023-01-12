c21ef1d6b4423adc7fba9e7ab7d32d7668a1bfec
Tanujarora100 <108284546+Tanujarora100@users.noreply.github.com>
2023-01-12 11:06:33 +0530

    Update implement_two_stacks.py

--- StackandQueue/implement_two_stacks.py
+++ StackandQueue/implement_two_stacks.py
@@ -1,46 +1,63 @@
-class TwoStack:
-    # Time Complexity
-    # O(1) for push operation, as it only takes constant time to push an element into the stack.
-    # O(1) for pop operation, as it only takes constant time to pop out an element out of the stack.
-    # Space Complexity O(N), where N is the size of the array used for two stack's implementation.
-    def __init__(self, s):
-        self.size = s
-        self.arr = [0] * self.size
+'''
+Time Complexity:
+O(1) for push operation, as it only takes constant time to push an element into the stack.
+O(1) for pop operation, as it only takes constant time to pop out an element out of the stack.
+Space Complexity:
+O(N), where N is the size of the array used for two stack's implementation.
+'''
+# One Stack-Left to Right
+# Two Stack-Right to Left
+
+
+class stack:
+    def __init__(self, size) -> None:
+        self.size = size
+        self.arr = [0]*self.size
         self.top1 = -1
+        # At the Beginning of the Array
         self.top2 = self.size
+        # At the end
 
-    # Push function for the first stack
-    def push1(self, val):
-        # If overflow exists, return
-        if self.top1 + 1 == self.top2:
-            return
+    def push1(self, value):
+        # Check for Overflow of stack
+        if self.top1+1 == self.top2:
+            return -1
         self.top1 += 1
-        # Ek aage badha do and vaha par insert kardo
-        self.arr[self.top1] = val
+        # Increment the index
+        self.arr[self.top1] = value
 
-    # Push function for the second stack
-    def push2(self, val):
-        # If overflow exists, return
-        if self.top2 - 1 == self.top1:
-            return
+    def push2(self, value):
+        # Check for Overflow of stack
+        if self.top2-1 == self.top1:
+            return -1
         self.top2 -= 1
-        # Right to left aa rahe hai to ek peeche leh jao index and insert kardo
-        self.arr[self.top2] = val
+        self.arr[self.top2] = value
 
-    # Pop function for the first stack
-    def pop1(self):
-        # If underflow exists, return -1
+    def pop1(self) -> int:
         if self.top1 == -1:
             return -1
-        # self.top1 -= 1
-        # stack1 is not empty, then we decrement top1 by 1 and return the value of Arr[top1+1].
-        return self.arr[self.top1 + 1]
+        # No element in the stack to pop
+        temp = self.arr[self.top1]
+        self.top1 -= 1
+        return temp
 
-    # Pop function for the second stack
-    def pop2(self):
-        # If underflow exists, return -1
+    def pop2(self) -> int:
+        # If the stack is empty
         if self.top2 == self.size:
             return -1
-
+        temp = self.arr[self.top2]
         self.top2 += 1
-        return self.arr[self.top2 - 1]
+        return temp
+
+    def print(self):
+        for i in range(len(self.arr)):
+            print(self.arr[i], end="  ")
+
+
+ob = stack(10)
+ob.push1(100)
+ob.push2(200)
+ob.push1(200)
+ob.push1(10000)
+ob.push2(56)
+ob.print()
